8db081bfa7a752b98b001f4d899908f93d8dd302
VitaliyBolgar <118684839+VitaliyBolgar@users.noreply.github.com>
2022-11-25 11:59:28 +0300

    добавил комментарии
    к задаче на js

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-var height = prompt("Введите высоту:");
-var width = prompt("Введите ширину:");
-var S = width*height;
-var text = "Площадь прямоугольника равна "+ S;
-alert(text)
+var chislo = prompt("Введите 3х значное число")
+// promt = 333
+var edinizy = chislo%10;
+// Х = 333%10 = 3
+var a = (chislo/10) - (edinizy*0.1);
+// Х = 333/10 - 3*0.1 = 33.3 - 0.3 = 33
+var deciatki = a%10;
+// X = 33%10 = 3
+sotni = (a/10) - (deciatki*0.1);
+// 33/10 - 3.3-3*0.1 = 3
+
+var text = sotni+", "+deciatki+", "+edinizy;
+alert(text);
