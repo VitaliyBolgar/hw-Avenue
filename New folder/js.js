064033598639e3e9c444c496a26e12214862d864
VitaliyBolgar <118684839+VitaliyBolgar@users.noreply.github.com>
2022-11-26 22:16:46 +0300

    Программа сложения +prompt

---
+++ New folder/js.js
@@ -0,0 +1,52 @@
+// let str = "какая-то строка"
+// let num = 1234
+// let bool = true
+// let notValue = null
+// let und = undefined
+// let app = []
+
+// let avenue = 1
+// let avenue = 2
+
+// console.log(avenue)
+
+// // + - сложение
+// // - - вычитание
+// // - /
+// // - *
+// // - ++
+// // - --
+// // - **
+// // - %
+
+// let chislo = prompt("Введите 3х значное число")
+// // 345 = chislo%10 = 5
+// let ediniza = chislo%10
+// let a = (chislo/10)-(ediniza*0.1)
+// let desiatki = a%10
+// //sotni
+// //вывод на экран
+
+// let num1 = prompt("ввеод 1");
+// let num2 = prompt("ввеод 2");
+
+
+// let num1 = 233;
+// let num2 = 134;
+// let result = (num1+num2);
+// let text= "Результат :"+result;
+// console.log(text);
+
+let num11 = +prompt("введите число");	
+let num22 = +prompt ("введите число");
+let result=(+num11+num22);
+let text = "Результат :"+result;
+
+alert(text);
+
+
+
+let value;
+
+
+value = String(10)
