4aa0186c30593384999e2b964dce9470e80e7131
VitaliyBolgar <118684839+VitaliyBolgar@users.noreply.github.com>
2022-11-30 22:58:10 +0300

    задача с if else

--- New folder/js.js
+++ New folder/js.js
@@ -37,17 +37,159 @@
 // let text= "Результат :"+result;
 // console.log(text);
 
-let num1 = +prompt()
-let num2 = +prompt()
-let num3 = +prompt()
-let num4 = +prompt()
-let num5 = +prompt()
-let num6 = +prompt()
+// let num1 = +prompt()
+// let num2 = +prompt()
+// let num3 = +prompt()
+// let num4 = +prompt()
+// let num5 = +prompt()
+// let num6 = +prompt()
 
-let result = num1+num2
-let result1 = num3+num4
-let result2 = num5+num6
+// let result = num1+num2
+// let result1 = num3+num4
+// let result2 = num5+num6
 
-// alert('Наш результат: '+result + 'Наш результат: '+result1+'Наш результат: '+result2)
-alert(`Наш результат: ${result} Наш результат: ${result1} Наш результат:${result2}`)
+// // alert('Наш результат: '+result + 'Наш результат: '+result1+'Наш результат: '+result2)
+// alert(`Наш результат: ${result} Наш результат: ${result1} Наш результат:${result2}`)
 
+// let numb1= prompt("введите 1 число")
+// let numb2 = prompt("введите 2 число")
+// alert(`result ${numb1%numb2}`)
+
+// let a = prompt("Введте первое число")
+// let b = prompt("Введте второе число")
+
+// if(a>b){
+// 	alert(`число ${a} больше чем число ${b}`)
+// } 
+// else if(a<b){
+// 	alert(`число ${b} больше чем число ${a}`)
+// }
+
+// if(10>100){
+// 	console.log(1)
+// }
+// 	else{
+// 		console.log(2)
+// 	}
+
+
+// let num1 = +prompt()
+// let num2 = +prompt()
+// let result = num1+num2
+// alert(result)
+
+
+// console.log(!(100=="100"))
+
+// let num1 = +prompt()
+// let num2 = +prompt()
+// let num3 = +prompt()
+// let num4 = +prompt()
+// let num5 = +prompt()
+// let num6 = +prompt()
+
+// let result1 = num1+num2
+// let result2 = num2+num4
+// let result3 = num5+num6
+
+// alert(`Наш результат: ${result1} Наш результат: ${result2} Наш результат: ${result3}`)
+
+
+// let name1 = prompt("Как зовут?")
+// alert(`Ваше имя ${name1}`)
+// '
+
+
+// let num11 = prompt()
+// let num22 = prompt()
+// alert(`${num11%num22}`)
+
+
+// let str = "строка"
+// let num = \\ Number
+// let bigint = "большое число"
+// let bool = "true\false"
+// let notValue = "null"
+// let undefined = ""
+
+
+
+// let res = 5
+// res *= 5
+// res = res +5
+// res++
+
+// console.log (res);
+
+// <= 
+// >=
+// == нестрогое сравнение
+// === строгое сравнение
+// > 
+// <
+// ! НЕ
+
+
+// console.log (10<=100);
+// console.log (10>=100);
+// console.log (100>=100);
+// console.log (100>100);
+// console.log (100==100);
+// console.log (100===100);
+// console.log (100==="100");
+// console.log (100=="100");
+// console.log (!(100=="100"));
+// console.log ((100<10)&&(10<100));
+// console.log ((100<10)&&(10<100));
+// console.log ((100<10)||(10<100));
+
+
+// let nun1 = prompt ("Введите первое число")
+// let nun2 = prompt ("Введите второе число")
+
+// if (nun1>nun2){
+//     alert(`первое число ${nun1} больше второго ${nun2}`)
+// }else {
+
+// }alert(`первое число ${nun1} меньше второго ${nun2}`)
+
+
+
+// let hours = prompt ("Задайте переменную") 
+// if (hours<=14){
+// 	alert("Первая четверть")
+// }
+// else if (hours>=15&&hours<=29) {
+// 	alert("Вторая четверть")
+// }
+// else if(hours>=30&&hours<=44) {
+// 	alert("Третья четверть")
+// }
+// else if(hours>=45&&hours<=59)
+// {
+// 	alert("Четвертая четверть")
+// }
+// else (hours<1 && hours>=60) 
+// {
+// 	alert("Неверное число")
+// }
+
+let hours = prompt ("Задайте переменную") 
+if (hours>=1 && hours<= 14)
+{
+	alert("Первая четверть")
+}
+else if (hours>=15&&hours<=29) {
+	alert("Вторая четверть")
+}
+else if(hours>=30&&hours<=44) {
+	alert("Третья четверть")
+}
+else if(hours>=45&&hours<=59)
+{
+	alert("Четвертая четверть")
+}
+else  
+{
+	alert("Неверное число")
+}
