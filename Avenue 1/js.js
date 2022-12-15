200f0c296ae563439b1a5badc880e4f9edb29b1b
VitaliyBolgar <118684839+VitaliyBolgar@users.noreply.github.com>
2022-12-15 14:25:36 +0300

    Update js.js

--- Avenue 1/js.js
+++ Avenue 1/js.js
@@ -1,232 +1,66 @@
-// let str = "какая-то строка"
-// let num = 1234
-// let bool = true
-// let notValue = null
-// let und = undefined
-// let app = []
+// let first = +prompt();
+// let second = +prompt();
 
-// let avenue = 1
-// let avenue = 2
-
-// console.log(avenue)
-
-// // + - сложение
-// // - - вычитание
-// // - /
-// // - *
-// // - ++
-// // - --
-// // - **
-// // - %
-
-// let chislo = prompt("Введите 3х значное число")
-// // 345 = chislo%10 = 5
-// let ediniza = chislo%10
-// let a = (chislo/10)-(ediniza*0.1)
-// let desiatki = a%10
-// //sotni
-// //вывод на экран
-
-// let num1 = prompt("ввеод 1");
-// let num2 = prompt("ввеод 2");
-
-
-// let num1 = 233;
-// let num2 = 134;
-// let result = (num1+num2);
-// let text= "Результат :"+result;
-// console.log(text);
-
-// let num1 = +prompt()
-// let num2 = +prompt()
-// let num3 = +prompt()
-// let num4 = +prompt()
-// let num5 = +prompt()
-// let num6 = +prompt()
-
-// let result = num1+num2
-// let result1 = num3+num4
-// let result2 = num5+num6
-
-// // alert('Наш результат: '+result + 'Наш результат: '+result1+'Наш результат: '+result2)
-// alert(`Наш результат: ${result} Наш результат: ${result1} Наш результат:${result2}`)
-
-// let numb1= prompt("введите 1 число")
-// let numb2 = prompt("введите 2 число")
-// alert(`result ${numb1%numb2}`)
-
-// let a = prompt("Введте первое число")
-// let b = prompt("Введте второе число")
-
-// if(a>b){
-// 	alert(`число ${a} больше чем число ${b}`)
-// } 
-// else if(a<b){
-// 	alert(`число ${b} больше чем число ${a}`)
+// function callSumm2(par1, par2) {
+// 	let summPar = par1 + par2;
+// 	return summPar;
 // }
+// let RezultS = callSumm2(first, second);
+// console.log(`Результат: ${RezultS}`);
 
-// if(10>100){
-// 	console.log(1)
-// }
-// 	else{
-// 		console.log(2)
-// 	}
+// const newArr = [111, 222, 333, 444, 555]
+// console.log(newArr)
+// const newArr2 = newArr.slice(2, 4)
+// console.log(newArr2)
 
+// // Метод Канкаn
+// тоже не изменяет наши массивы
+// const arr1 = [1, 2, 3, 4, 56, 78, 88];
+// const arr2 = [1, 2, 3];
+// console.log(arr1.concat(arr2))
 
-// let num1 = +prompt()
-// let num2 = +prompt()
-// let result = num1+num2
-// alert(result)
-
-
-// console.log(!(100=="100"))
-
-// let num1 = +prompt()
-// let num2 = +prompt()
-// let num3 = +prompt()
-// let num4 = +prompt()
-// let num5 = +prompt()
-// let num6 = +prompt()
-
-// let result1 = num1+num2
-// let result2 = num2+num4
-// let result3 = num5+num6
-
-// alert(`Наш результат: ${result1} Наш результат: ${result2} Наш результат: ${result3}`)
+// const arr3 = arr1.concat(arr2);
 
 
-// let name1 = prompt("Как зовут?")
-// alert(`Ваше имя ${name1}`)
-// '
+// forEach метод который перебирает наш массив
+// нельзя применять к строке (метод массива)
+// => стрелочная функция??
+// есть 3 значения. 1(item) - каждый эл массива по очереди, 2(index) - индекс (0,1,2,3,4,5,6), 3(array) - вывод массива
+// обычно item || index
+
+// const newArr = [111, 222, 333, 444, 555]
+// newArr.splice(0, 2, 2222222, 55555)
+// // delete newArr[2]
+// console.log(newArr);
+// console.log(newArr.length);
 
 
-// let num11 = prompt()
-// let num22 = prompt()
-// alert(`${num11%num22}`)
-
-
-// let str = "строка"
-// let num = \\ Number
-// let bigint = "большое число"
-// let bool = "true\false"
-// let notValue = "null"
-// let undefined = ""
+// let programma = prompt("Введте ваш возвраст")
 
 
 
-// let res = 5
-// res *= 5
-// res = res +5
-// res++
 
-// console.log (res);
+let age = prompt()
 
-// <= 
-// >=
-// == нестрогое сравнение
-// === строгое сравнение
-// > 
-// <
-// ! НЕ
+function changeAge(wAge) {
+let wAge = (age <= 3) ? 'такой молодой?' :
+	(age > 3 && age <= 13) ? "Скоро 14" :
+		age == 14 ? 'пора идти за паспортом' :
+	(age > 14 && age < 49) ? "учись трудись работой брат" :
+	(age >= 50 && age <= 114) ? "Тут плоды будут пожинаться" :
+						'Это очень странно';
+	return(wAge)
+}
 
 
-// console.log (10<=100);
-// console.log (10>=100);
-// console.log (100>=100);
-// console.log (100>100);
-// console.log (100==100);
-// console.log (100===100);
-// console.log (100==="100");
-// console.log (100=="100");
-// console.log (!(100=="100"));
-// console.log ((100<10)&&(10<100));
-// console.log ((100<10)&&(10<100));
-// console.log ((100<10)||(10<100));
+// let ageN2 
 
-
-// let nun1 = prompt ("Введите первое число")
-// let nun2 = prompt ("Введите второе число")
-
-// if (nun1>nun2){
-//     alert(`первое число ${nun1} больше второго ${nun2}`)
-// }else {
-
-// }alert(`первое число ${nun1} меньше второго ${nun2}`)
-
-
-
-// let hours = prompt ("Задайте переменную") 
-// if (hours<=14){
-// 	alert("Первая четверть")
-// }
-// else if (hours>=15&&hours<=29) {
-// 	alert("Вторая четверть")
-// }
-// else if(hours>=30&&hours<=44) {
-// 	alert("Третья четверть")
-// }
-// else if(hours>=45&&hours<=59)
-// {
-// 	alert("Четвертая четверть")
-// }
-// else (hours<1 && hours>=60) 
-// {
-// 	alert("Неверное число")
-// }
-
-// let hours = prompt ("Задайте переменную") 
-// if (hours>=1 && hours<= 14)
-// {
-// 	alert("Первая четверть")
-// }
-// else if (hours>=15&&hours<=29) {
-// 	alert("Вторая четверть")
-// }
-// else if(hours>=30&&hours<=44) {
-// 	alert("Третья четверть")
-// }
-// else if(hours>=45&&hours<=59)
-// {
-// 	alert("Четвертая четверть")
-// }
-// else  
-// {
-// 	alert("Неверное число")
-// }
-
-
-// let ru = ['пн вт ср чт пт сб вс']
-// let en = ['m th w ther f std sd']
-
-// let lang = prompt("Ввод переменной")
-// if (lang == "ru") 
-// {
-// 	alert (ru)
-// }
-// else if (lang == "en")
-// {
-// 	alert (en)
-// }
-// else
-// {
-// 	alert("Неверное значение")
-// }
-
-
-// let value;
-// value = String(10);
-// value = (40).toString();
-
-// value= Number(55+"55")
-
-// console.log(value);
-// console.log(typeof value);
-
-let lang = prompt ('Набери язык (ru; en)') 
-let arr = { 
- 'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'], 
- 'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-
-}; 
-if(!(lang=='ru&&en'))
-alert('неизвестное число')
+alert(changeAge())
+	
+let t = age
+while (t < 5) {
+	changeAge()
+	console.log(t)
+	t++
+}
+	// ? alert('Круто!') : alert("Нет!")
